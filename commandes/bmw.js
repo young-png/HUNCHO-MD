b52d621dd925786574e762cddb1ee2d12ea52972
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-05-19 07:54:09 +0300

    HUNCHO

--- commandes/bmw.js
+++ commandes/bmw.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "bmw", reaction: "🚗", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "huncho", reaction: "🚗", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '𝒀𝒆𝒔😉 𝐚𝐦 𝐇𝐔𝐍𝐂𝐇𝐎 𝐌𝐃 𝐜𝐫𝐞𝐚𝐭𝐞𝐝 𝐛𝐲 𝐊𝐞𝐢𝐭𝐡 𝐊𝐞𝐢𝐳𝐳𝐚𝐡\n\n ' + "𝐈𝐟 𝐲𝐨𝐮 𝐰𝐚𝐧𝐧𝐚 𝐮𝐬𝐞 𝐦𝐞 𝐣𝐮𝐬𝐭 𝐭𝐲𝐩𝐞 𝐦𝐞𝐧𝐮 𝐭𝐨 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 𝐦𝐞";
     let d = '                                                🏁𝐎𝐟 𝐜𝐨𝐮𝐫𝐬𝐞 𝐚𝐦 𝐭𝐡𝐞 𝐛𝐞𝐬𝐭 𝐛𝐨𝐭 𝐢𝐧 𝐭𝐡𝐞 𝐮𝐧𝐢𝐯𝐞𝐫𝐬𝐞';
